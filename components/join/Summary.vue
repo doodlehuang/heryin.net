cd21c77a66abe5b64247bc31946d95d53bc24127
纾浚 <junical@email.cn>
2023-02-18 01:13:16 +0800

    加入初步起步

---
+++ components/join/Summary.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div id="contact" class="max-w-6xl mx-auto px-4 sm:px-6">
+      <div class="pb-12 md:pb-20">
+        <div
+          class="bg-primary rounded py-10 px-8 md:py-12 md:px-12 shadow-2xl"
+          data-aos="zoom-y-out"
+        >
+          <div class="flex flex-col justify-between items-center">
+            <h3 class="mb-6 h3 text-white text-center mb-2">
+              加入和瑛社，<br />
+              熔铸兴趣与热爱，施展才华和本领。汲取知识、开启思路。<br />
+              尽享优势资源，站位高端平台。同我们一起成功！<br />
+            </h3>
+            <NuxtLink
+              class="btn btn-lg btn-wide text-primary bg-gradient-to-r from-red-100 to-white"
+              to="/join"
+            >
+              加入和瑛
+            </NuxtLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ pages/join.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <JoinHero />
+    <JoinZhuxi />
+    <JoinSummary />
+  </div>
+</template>
