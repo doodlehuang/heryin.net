5aea4de4a06c4079659d32a6a57b4af3acecdbdf
纾浚 <junical@email.cn>
2023-03-07 01:24:02 +0800

    start

---
+++ pages/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>home</div>
+  <ContentList path="/blog" v-slot="{ list }">
+    <div
+      class="card w-96 bg-base-100 shadow-xl"
+      v-for="article in list"
+      :key="article._path"
+    >
+      <div class="card-body">
+        <h2 class="card-title">{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">阅读全文</button>
+        </div>
+      </div>
+    </div>
+  </ContentList>
+</template>
