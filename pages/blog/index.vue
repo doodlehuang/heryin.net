b1872503378fd3d14cb75045df9d91fceae7de8e
纾浚 <junical@email.cn>
2023-05-05 08:41:23 +0800

    文章列表

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,18 +1,40 @@
 <template>
-  <div>home</div>
-  <ContentList path="/blog" v-slot="{ list }">
-    <div
-      class="card w-96 bg-base-100 shadow-xl"
-      v-for="article in list"
-      :key="article._path"
-    >
-      <div class="card-body">
-        <h2 class="card-title">{{ article.title }}</h2>
-        <p>{{ article.description }}</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">阅读全文</button>
-        </div>
+  <!-- <div>{{ contentQuery }}</div> -->
+  <div
+    class="card w-96 bg-base-100 shadow-xl"
+    v-for="article in list"
+    :key="article.link"
+  >
+    <div class="card-body">
+      <h2 class="card-title">{{ article.title }}</h2>
+      <p>{{ article.desc }}</p>
+      <div class="card-actions justify-end">
+        <NuxtLink class="btn btn-primary" :to="article.link">阅读全文</NuxtLink>
       </div>
     </div>
-  </ContentList>
+  </div>
 </template>
+
+<script setup lang="ts">
+type Article = {
+  title: string
+  date: Date
+  desc: string
+  link: string
+}
+
+const contentQuery = await queryContent().find()
+const list = reactive([] as Article[])
+for (const article of contentQuery) {
+  list.push({
+    title: article.title || '无标题',
+    date: new Date(article.date),
+    desc: article.description,
+    link: article._path || '#',
+  })
+}
+
+list.sort((a, b) => (a.date > b.date ? -1 : 1))
+
+console.log(list)
+</script>
