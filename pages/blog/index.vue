3fadae261cec7a345ac90599aac531f119d79a15
纾浚 <junical@email.cn>
2023-06-08 03:24:31 +0800

    blog功能初步完工

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,16 +1,28 @@
 <template>
-  <!-- <div>{{ contentQuery }}</div> -->
-  <div
-    class="card w-96 bg-base-100 shadow-xl"
-    v-for="article in list"
-    :key="article.link"
-  >
-    <div class="card-body">
-      <h2 class="card-title">{{ article.title }}</h2>
-      <p>{{ article.desc }}</p>
-      <div class="card-actions justify-end">
-        <NuxtLink class="btn btn-primary" :to="article.link">阅读全文</NuxtLink>
-      </div>
+  <div class="flex flex-col items-center pt-24 pb-10">
+    <!-- <h1>和瑛博客</h1> -->
+    <div
+      class="card w-11/12 md:w-3/5 lg:1/2 bg-white shadow-lg hover:shadow-xl transition duration-500 ease-in-out m-4"
+      v-for="article in list"
+      :key="article.link"
+    >
+      <NuxtLink :to="article.link">
+        <div class="card-body">
+          <h2
+            class="card-title text-primary text-2xl md:text-3xl leading-tight"
+            style="font-family: Heryin"
+          >
+            {{ article.title }}
+          </h2>
+          <p class="text-gray-600 text-sm mb-2">
+            发表于 {{ article.date.toLocaleDateString() }}
+          </p>
+          <p>{{ article.desc }}</p>
+          <div class="card-actions justify-end">
+            <!-- <button class="link link-primary">阅读全文</button> -->
+          </div>
+        </div>
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -24,6 +36,7 @@ type Article = {
 }
 
 const contentQuery = await queryContent().find()
+
 const list = reactive([] as Article[])
 for (const article of contentQuery) {
   list.push({
@@ -35,6 +48,4 @@ for (const article of contentQuery) {
 }
 
 list.sort((a, b) => (a.date > b.date ? -1 : 1))
-
-console.log(list)
 </script>
